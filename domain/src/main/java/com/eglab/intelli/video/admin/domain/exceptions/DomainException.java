a1fb311cda41878865c8d8a03b94bc11c35b108f
eliasgermoliato <eliasgermoliato@hotmail.com>
2024-10-05 18:04:15 -0300

    refactor: adicionando NoStacktraceException e melhorando a construção das exceptions

--- domain/src/main/java/com/eglab/intelli/video/admin/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/eglab/intelli/video/admin/domain/exceptions/DomainException.java
@@ -3,18 +3,23 @@ package com.eglab.intelli.video.admin.domain.exceptions;
 import java.util.List;
 import com.eglab.intelli.video.admin.domain.validation.Error;
 
-public class DomainException extends RuntimeException {
+public class    DomainException extends NoStacktraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors){
-        super("", null, true, false);
+    private DomainException(final String aMessage, final List<Error> anErrors){
+        super(aMessage);
         this.errors = anErrors;
     }
 
-    public static DomainException with(final List<Error> anErrors){
-        return new DomainException(anErrors);
+    public static DomainException with(final Error anErrors){
+        return new DomainException(anErrors.message(), List.of(anErrors));
     }
+
+    public static DomainException with(final List<Error> anErrors){
+        return new DomainException("", anErrors);
+    }
+
 
     public List<Error> getErrors(){
         return errors;
